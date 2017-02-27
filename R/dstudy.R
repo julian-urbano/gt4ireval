bc5f0bd0d74cfa25101c99a1d7f29758f94a7884
julian-urbano <urbano.julian@gmail.com>
2017-02-27 17:33:04 +0100

    Reformatting of code.

--- R/dstudy.R
+++ R/dstudy.R
@@ -1,8 +1,19 @@
-d.study <- function(gdata, queries = gdata$n.q, stability = 0.95, alpha = 0.025) {
-  if(length(alpha) > 1 & length(queries) > 1)
+## Copyright (C) 2013-2017  Julián Urbano <urbano.julian@gmail.com>
+## Distributed under the terms of the MIT License.
+
+dstudy <- function(gdata, queries = gdata$n.q, stability = 0.95, alpha = 0.025) {
+  if(!inherits(gdata, "gstudy"))
+    stop("gdata is not a valid g-study object.")
+  if(length(alpha) > 1 && length(queries) > 1)
     stop("Only one of 'alpha' and 'queries' may have multiple values")
-  if(length(alpha) > 1 & length(stability) > 1)
+  if(length(alpha) > 1 && length(stability) > 1)
     stop("Only one of 'alpha' and 'stability' may have multiple values")
+  if(any(queries < 1))
+    stop("Number of queries must be one or more")
+  if(any(stability <= 0 | stability >= 1))
+    stop("Target stability must be in (0,1)")
+  if(any(alpha <= 0 | alpha >= 1))
+    stop("Significance level must be in (0,1)")
 
   # Point estimates for the indicated n.q
   Erho2_ <- gdata$var.s / (gdata$var.s + gdata$var.e / queries)
@@ -29,7 +40,7 @@ d.study <- function(gdata, queries = gdata$n.q, stability = 0.95, alpha = 0.025)
   Phi.lwr <- gdata$em.s^2 - qf(1-alpha, df.s, Inf) * gdata$em.s * gdata$em.e
   Phi.lwr <- Phi.lwr + (qf(1-alpha, df.s, Inf) - qf(1-alpha, df.s, df.e)) *
     qf(1-alpha, df.s, df.e) * gdata$em.e^2
-  Phi.lwr <- Phi.lwr / ( (gdata$n.s - 1) * qf(1-alpha, df.s, Inf,) * gdata$em.s * gdata$em.e +
+  Phi.lwr <- Phi.lwr / ( (gdata$n.s - 1) * qf(1-alpha, df.s, Inf) * gdata$em.s * gdata$em.e +
                            qf(1-alpha, df.s, df.q) * gdata$em.s * gdata$em.q )
   Phi.lwr <- gdata$n.s * Phi.lwr / (gdata$n.s * Phi.lwr + gdata$n.q)
   n.q_Phi.lwr <- ceiling(stability * (1 - Phi.lwr) / (Phi.lwr * (1 - stability)))
@@ -38,7 +49,7 @@ d.study <- function(gdata, queries = gdata$n.q, stability = 0.95, alpha = 0.025)
   Phi.upr <- gdata$em.s^2 - qf(alpha, df.s, Inf) * gdata$em.s * gdata$em.e
   Phi.upr <- Phi.upr + (qf(alpha, df.s, Inf) - qf(alpha, df.s, df.e)) *
     qf(alpha, df.s, df.e) * gdata$em.e^2
-  Phi.upr <- Phi.upr / ( (gdata$n.s - 1) * qf(alpha, df.s, Inf,) * gdata$em.s * gdata$em.e +
+  Phi.upr <- Phi.upr / ( (gdata$n.s - 1) * qf(alpha, df.s, Inf) * gdata$em.s * gdata$em.e +
                            qf(alpha, df.s, df.q) * gdata$em.s * gdata$em.q )
   Phi.upr <- gdata$n.s * Phi.upr / (gdata$n.s * Phi.upr + gdata$n.q)
   n.q_Phi.upr <- ceiling(stability * (1 - Phi.upr) / (Phi.upr * (1 - stability)))
@@ -59,7 +70,7 @@ d.study <- function(gdata, queries = gdata$n.q, stability = 0.95, alpha = 0.025)
   return(res)
 }
 
-print.dstudy <- function(x) {
+print.dstudy <- function(x, ...) {
   cat("\nSummary of D-Study\n")
   cat("\nCall:\n")
   cat("    queries =", x$call$queries, "\n")
@@ -73,15 +84,29 @@ print.dstudy <- function(x) {
     cat("             -----------------------------------   -----------------------------------\n")
     cat("       Alpha    Expected       Lower       Upper      Expected       Lower       Upper\n")
     cat(" ----------- ----------- ----------- -----------   ----------- ----------- -----------\n")
-    for(i in 1:length(x$call$alpha))
-      cat("", sprintf("%*.5g",11,x$call$alpha[i]), sprintf("%*.5g",11,x$Erho2[1]), sprintf("%*.5g",11,x$Erho2.lwr[i]), sprintf("%*.5g",11,x$Erho2.upr[i]), " ", sprintf("%*.5g",11,x$Phi[1]), sprintf("%*.5g",11,x$Phi.lwr[i]), sprintf("%*.5g",11,x$Phi.upr[i]), "\n")
+    for(i in seq_along(x$call$alpha))
+      cat("", sprintf("%*.5g", 11, x$call$alpha[i]),
+          sprintf("%*.5g", 11, x$Erho2[1]),
+          sprintf("%*.5g", 11, x$Erho2.lwr[i]),
+          sprintf("%*.5g", 11, x$Erho2.upr[i]),
+          " ",
+          sprintf("%*.5g", 11, x$Phi[1]),
+          sprintf("%*.5g", 11, x$Phi.lwr[i]),
+          sprintf("%*.5g", 11, x$Phi.upr[i]), "\n")
   }else{
     cat("                                           Erho2                                   Phi\n")
     cat("             -----------------------------------   -----------------------------------\n")
     cat("     Queries    Expected       Lower       Upper      Expected       Lower       Upper\n")
     cat(" ----------- ----------- ----------- -----------   ----------- ----------- -----------\n")
-    for(i in 1:length(x$call$queries))
-      cat("", sprintf("%*.5g",11,x$call$queries[i]), sprintf("%*.5g",11,x$Erho2[i]), sprintf("%*.5g",11,x$Erho2.lwr[i]), sprintf("%*.5g",11,x$Erho2.upr[i]), " ", sprintf("%*.5g",11,x$Phi[i]), sprintf("%*.5g",11,x$Phi.lwr[i]), sprintf("%*.5g",11,x$Phi.upr[i]), "\n")
+    for(i in seq_along(x$call$queries))
+      cat("", sprintf("%*.5g", 11, x$call$queries[i]),
+          sprintf("%*.5g", 11, x$Erho2[i]),
+          sprintf("%*.5g", 11, x$Erho2.lwr[i]),
+          sprintf("%*.5g", 11, x$Erho2.upr[i]),
+          " ",
+          sprintf("%*.5g", 11, x$Phi[i]),
+          sprintf("%*.5g", 11, x$Phi.lwr[i]),
+          sprintf("%*.5g", 11, x$Phi.upr[i]), "\n")
   }
 
   # Number of queries
@@ -91,14 +116,28 @@ print.dstudy <- function(x) {
     cat("             -----------------------------------   -----------------------------------\n")
     cat("       Alpha    Expected       Lower       Upper      Expected       Lower       Upper\n")
     cat(" ----------- ----------- ----------- -----------   ----------- ----------- -----------\n")
-    for(i in 1:length(x$call$alpha))
-      cat("", sprintf("%*.5g",11,x$call$alpha[i]), sprintf("%*.5g",11,x$n.q_Erho2[1]), sprintf("%*.5g",11,x$n.q_Erho2.upr[i]), sprintf("%*.5g",11,x$n.q_Erho2.lwr[i]), " ", sprintf("%*.5g",11,x$n.q_Phi[1]), sprintf("%*.5g",11,x$n.q_Phi.upr[i]), sprintf("%*.5g",11,x$n.q_Phi.lwr[i]), "\n")
+    for(i in seq_along(x$call$alpha))
+      cat("", sprintf("%*.5g", 11, x$call$alpha[i]),
+          sprintf("%*.5g", 11, x$n.q_Erho2[1]),
+          sprintf("%*.5g", 11, x$n.q_Erho2.upr[i]),
+          sprintf("%*.5g", 11, x$n.q_Erho2.lwr[i]),
+          " ",
+          sprintf("%*.5g", 11, x$n.q_Phi[1]),
+          sprintf("%*.5g", 11, x$n.q_Phi.upr[i]),
+          sprintf("%*.5g", 11, x$n.q_Phi.lwr[i]), "\n")
   }else{
     cat("                                           Erho2                                   Phi\n")
     cat("             -----------------------------------   -----------------------------------\n")
     cat("   Stability    Expected       Lower       Upper      Expected       Lower       Upper\n")
     cat(" ----------- ----------- ----------- -----------   ----------- ----------- -----------\n")
-    for(i in 1:length(x$call$stability))
-      cat("", sprintf("%*.5g",11,x$call$stability[i]), sprintf("%*.5g",11,x$n.q_Erho2[i]), sprintf("%*.5g",11,x$n.q_Erho2.upr[i]), sprintf("%*.5g",11,x$n.q_Erho2.lwr[i]), " ", sprintf("%*.5g",11,x$n.q_Phi[i]), sprintf("%*.5g",11,x$n.q_Phi.upr[i]), sprintf("%*.5g",11,x$n.q_Phi.lwr[i]), "\n")
+    for(i in seq_along(x$call$stability))
+      cat("", sprintf("%*.5g", 11, x$call$stability[i]),
+          sprintf("%*.5g", 11, x$n.q_Erho2[i]),
+          sprintf("%*.5g", 11, x$n.q_Erho2.upr[i]),
+          sprintf("%*.5g", 11, x$n.q_Erho2.lwr[i]),
+          " ",
+          sprintf("%*.5g", 11, x$n.q_Phi[i]),
+          sprintf("%*.5g", 11, x$n.q_Phi.upr[i]),
+          sprintf("%*.5g", 11, x$n.q_Phi.lwr[i]), "\n")
   }
 }
