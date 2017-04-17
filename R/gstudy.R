f6761bc521ec2a1b58b09b78ffdee0f22bfd956f
julian-urbano <urbano.julian@gmail.com>
2017-04-17 02:31:33 +0200

    Decide whether to set negative estimates to zero (close #6).

--- R/gstudy.R
+++ R/gstudy.R
@@ -6,19 +6,17 @@
 #'
 #' @param data A data frame or matrix with the existing effectiveness scores. Systems are columns
 #'   and queries are rows.
+#' @param ensurePositive Whether to set negative variance estimates to zero or not. Defaults to
+#'   \code{FALSE}.
 #' @param drop The fraction of worst-performing systems to drop from the data before analysis.
 #'   Defaults to 0 (include all systems).
 #'
-#' @return An object of class \code{gstudy}, with the following components:
-#' \tabular{ll}{
-#' \code{n.s}, \code{n.q} \tab Number of systems and number of queries of the existing data. \cr
-#' \code{var.s}, \code{var.q}, \code{var.e} \tab Variance of the system, query, and residual
-#'   effects. \cr
-#' \code{em.s}, \code{em.q}, \code{em.e} \tab Mean squares of the system, query and residual
-#'   components. \cr
-#' \code{call} \tab A list with the existing \code{data} and the percentage of systems to
-#'   \code{drop}.\cr
-#' }
+#' @return An object of class \code{gstudy}, with the following components: \tabular{ll}{
+#'   \code{n.s}, \code{n.q} \tab Number of systems and number of queries of the existing data. \cr
+#'   \code{var.s}, \code{var.q}, \code{var.e} \tab Variance of the system, query, and residual
+#'   effects. \cr \code{em.s}, \code{em.q}, \code{em.e} \tab Mean squares of the system, query and
+#'   residual components. \cr \code{call} \tab A list with the existing \code{data} and the
+#'   percentage of systems to \code{drop}.\cr }
 #'
 #' @seealso \code{\link{dstudy}}
 #' @author Julián Urbano
@@ -34,7 +32,7 @@
 #' g20 <- gstudy(adhoc3, drop = 0.2)
 #' @export
 #' @importFrom stats quantile
-gstudy <- function(data, drop = 0) {
+gstudy <- function(data, ensurePositive = TRUE, drop = 0) {
   if(drop < 0 || drop >= 1)
     stop("Fraction of worst systems to drop must be in [0,1)")
   if(!is.data.frame(data) && !is.matrix(data))
@@ -62,24 +60,24 @@ gstudy <- function(data, drop = 0) {
   var.s <- (em.s - em.e) / n.q
   var.q <- (em.q - em.e) / n.s
   var.e <- em.e
-  if(var.s < 0){
-    var.s <- 0
+  if(var.s < 0)
     warning("Estimate of system variance is negative")
-  }
-  if(var.q < 0){
-    var.q <- 0
+  if(var.q < 0)
     warning("Estimate of query variance is negative")
-  }
-  if(var.e < 0){
-    var.e <- 0
+  if(var.e < 0)
     warning("Estimate of interaction variance is negative")
+
+  if(ensurePositive) {
+    var.s <- max(0, var.s)
+    var.q <- max(0, var.q)
+    var.e <- max(0, var.e)
   }
 
   res <- list(
     n.s = n.s, n.q = n.q,
     var.s = var.s, var.q = var.q, var.e = var.e,
     em.s = em.s, em.q = em.q, em.e = em.e,
-    call = list(data = data, drop = drop)
+    call = list(data = data, drop = drop, ensurePositive = ensurePositive)
   )
   class(res) <- "gstudy"
   return(res)
